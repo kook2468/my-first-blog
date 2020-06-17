564cc9884a7ab9508ff5cee295f2657573e2205a
kook2468 <cdg9808@naver.com>
2020-06-17 14:53:23 +0900

    first commit

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 from chart import views                                     # !!!
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('chart.urls')),
     path('world-population/',
          views.world_population, name='world_population'),  # !!!
-    path('admin/', admin.site.urls),
 ]
