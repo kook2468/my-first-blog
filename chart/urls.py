b52e996952d2ecb4408a9d390017b9cf3f1ae36d
kook2468 <cdg9808@naver.com>
2020-06-17 17:17:45 +0900

    first commit

--- chart/urls.py
+++ chart/urls.py
@@ -5,11 +5,11 @@ from chart import views                                     # !!!
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ticket-class/-2/',
+    path('ticket-class/1/',
          views.ticket_class_view_1, name='ticket_class_view_1'),
-    path('ticket-class/-1/',
+    path('ticket-class/2/',
          views.ticket_class_view_2, name='ticket_class_view_2'),
-    path('ticket-class/0/',
+    path('ticket-class/3/',
          views.ticket_class_view_3, name='ticket_class_view_3'),
     path('world-population/',
          views.world_population, name='world_population'),  # !!!
